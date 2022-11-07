9d1aaf0a036a953c48e4a2303a2cdd27c9175856
a8trejo <abel130@hotmail.com>
2022-11-07 15:24:58 -0800

    debugging

--- cypress/e2e/HeroApp/HeroAppSteps.js
+++ cypress/e2e/HeroApp/HeroAppSteps.js
@@ -3,19 +3,18 @@
 import { Given, When, Then, And } from "@badeball/cypress-cucumber-preprocessor"
 import HeroAppQuery from '../../support/Selectors/HeroAppQuery';
 
-// I click on the "Basic Auth" demo option
-before(() => {
+When('I click on the {string} demo option', (option) => {
     // Define and Encode the authentication string
     const authString = `${Cypress.env("username")}:${Cypress.env("password")}`;
+    cy.logMsg(authString)
+    
     const encodedAuth = Buffer.from(authString).toString('base64')
 
     // Preparing to intercept the /basic_auth request and add the auth header once we click on login
     cy.intercept('GET', '**/basic_auth', (req) => {
         req.headers['authorization'] = `Basic ${encodedAuth}`
     })
-})
 
-When('I click on the {string} demo option', (option) => {
     HeroAppQuery.selectors(option).click()
 })
 
