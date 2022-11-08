fb00adc93a3eb6e558c80d93bd375042c5bb3728
a8trejo <abel130@hotmail.com>
2022-11-08 09:10:02 -0800

    Debugging

--- .github/scripts/github-secrets.sh
+++ .github/scripts/github-secrets.sh
@@ -3,14 +3,16 @@ echo "$1"
 SECRETS_LIST=$(cat .env)
 ENV_KEY=$(echo $1 | tr '[:lower:]' '[:upper:]')
 
-echo $(cat .env)
+echo $GITHUB_ENV
 echo "----------------------------------------------------------------------"
 
 while IFS= read -r secret; do
     echo $secret | sed "s/=/_$ENV_KEY=/g"
     SECRET_CYPRESS=$(echo $secret | sed "s/=/_$ENV_KEY=\"/g" | sed "s/$/\"/")
-    # SECRET_CYPRESS=$(echo $secret | sed "s/=/_$ENV_KEY=/g" | sed "s/'//g")
-    export $SECRET_CYPRESS
+
+    # echo "PASS=something" >> $GITHUB_ENV
+    echo $SECRET_CYPRESS >> $GITHUB_ENV
+    # export $SECRET_CYPRESS
   
 done <<< "$SECRETS_LIST"
 
