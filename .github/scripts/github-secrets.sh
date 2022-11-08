677980f4ded767bed4ecbc352e39ad9e087b737b
a8trejo <abel130@hotmail.com>
2022-11-08 08:38:10 -0800

    Changing to bin bash

--- .github/scripts/github-secrets.sh
+++ .github/scripts/github-secrets.sh
@@ -1,13 +1,11 @@
-#!/bin/sh
+#!usr/bin/env bash
 
 echo "$1"
 
 SECRETS_LIST=$(cat .env)
 ENV_KEY=$(echo $1 | tr '[:lower:]' '[:upper:]')
-echo $ENV_KEY
+
 while IFS= read -r secret; do
-    
-    # echo "USER_$()=${{ secrets.CYPRESS_USERNAME }}"
 
     SECRET_CYPRESS=$(echo $secret | sed "s/=/_$ENV_KEY=/g" | sed "s/'//g")
     export $SECRET_CYPRESS
